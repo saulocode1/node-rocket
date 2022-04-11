8eaade7899af64fbb53c92711e2b0c1cf1a5cfa7
Saulo <sauloveigr@hotmail.com>
2022-04-10 23:31:25 -0300

    finish create account

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Requisitos
 
-   -  Criar conta
+   - [X] Criar conta
 
 ### Regras de negócio (o que não pode)
 
-   -  Cadastrar com CPF existente
+   - [X] Não cadastrar com CPF existente
--- src/index.js
+++ src/index.js
@@ -11,18 +11,25 @@ const { v4: uuidv4 } = require("uuid"); // (v4 - números randômicos)
 const app = express();
 
 app.use(express.json());
-
+   
 const costumers = [];
 
 app.post("/account", (request, response) => {
    const { cpf, name } = request.body;
 
-   const id = uuidv4();
+   const costumerAlreadyExists = costumers.some(
+      // some - procura no array e retorna true se existir
+      (costumer) => costumer.cpf === cpf
+   );
+
+   if (costumerAlreadyExists) {
+      return response.status(400).json({ error: "Costumer already exists!" });
+   }
 
    costumers.push({
       cpf,
       name,
-      id,
+      id: uuidv4(),
       statement: [],
    });
 
